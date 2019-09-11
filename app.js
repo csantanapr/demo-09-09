35cdcad1123c81652426f404545b104345632794
Carlos Santana <csantanapr@apache.org>
2019-09-11 17:01:28 -0400

    running another demo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Hema and Bryan");
+  res.send("Hello from Appsody Demo");
 });
 
 module.exports.app = app;
